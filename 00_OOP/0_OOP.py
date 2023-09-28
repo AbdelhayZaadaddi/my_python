335ae7499cfe83025909e04be6688734e24b0948
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-28 12:57:19 +0100

    oop

---
+++ 00_OOP/0_OOP.py
@@ -0,0 +1,21 @@
+class Emplayee:
+    pass
+
+emp_1 = Emplayee()
+emp_2 = Emplayee()
+
+# print(emp_1)
+# print(emp_2)
+
+emp_1.first = 'abde'
+emp_1.last = 'zadd'
+emp_1.email = 'abdelhay@gmail.com'
+emp_1.pay = 10000
+
+emp_2.first = 'gdgr'
+emp_2.last = 'zagrdd'
+emp_2.email = 'abdezflhay@gmail.com'
+emp_2.pay = 100
+
+print(emp_1.email)
+print(emp_2.pay)
