5c9943f60f397a1ed87f71679f0ff0ca12d3496e
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-28 14:02:03 +0100

    oop

--- 00_OOP/0_OOP.py
+++ 00_OOP/0_OOP.py
@@ -1,21 +1,17 @@
 class Emplayee:
-    pass
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
 
-emp_1 = Emplayee()
-emp_2 = Emplayee()
+    def Fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    def Email(self):
+        return self.email
+emp_1 = Emplayee('Abdelhay', 'Zaadaddi', 11000)
+emp_2 = Emplayee('lhay', 'daddi', 11000)
 
-# print(emp_1)
-# print(emp_2)
-
-emp_1.first = 'abde'
-emp_1.last = 'zadd'
-emp_1.email = 'abdelhay@gmail.com'
-emp_1.pay = 10000
-
-emp_2.first = 'gdgr'
-emp_2.last = 'zagrdd'
-emp_2.email = 'abdezflhay@gmail.com'
-emp_2.pay = 100
-
-print(emp_1.email)
-print(emp_2.pay)
+fullname = fullname()
+print(fullname)
+print(emp_2)
