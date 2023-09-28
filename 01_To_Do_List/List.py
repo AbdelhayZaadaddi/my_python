9e31260929c42c22f07db31b79db56d3c493e86f
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-28 16:25:45 +0100

    to do list

--- 01_To_Do_List/List.py
+++ 01_To_Do_List/List.py
@@ -1,8 +1,18 @@
 
 # Starting Creating list of the to do list
 
-my_list = ["Abdelhay"]
+my_list = ["Abdelhay", "zzadaddi"]
+
+# adding element to the to do list
 new_element = input("Please enter something to the lest:  ")
+trc = 0
 my_list.append(new_element)
 for element in my_list:
-    print(my_list, end = " ")
+    trc += 1
+    print("{} : {}".format(trc, element))
+
+# removing element from the todo list
+rm = int(input("Please enter the number of the To do u wanna to remove")) - 1
+my_list.remove(trc)
+for element in my_list:
+    print('{} : {}'.format(trc, element))
