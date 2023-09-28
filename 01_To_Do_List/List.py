0d48bbeb90ed540db924f979c6fe6824c03548d5
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-28 14:26:06 +0100

    starting working on it

---
+++ 01_To_Do_List/List.py
@@ -0,0 +1,5 @@
+
+
+# Starting Creating list of the to do list
+my_list = []
+print("")
---
+++ 01_To_Do_List/Login.py
@@ -0,0 +1,3 @@
+def login():
+    usr_name = input("Please neter your user name: ")
+    usr_password = input("Please enter your password: ")
