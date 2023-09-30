8cf6648a5d9f3859f29df20cd0b626f509f5869c
AbdelhayZaadaddi <abdezaadaddi06@gmail.com>
2023-09-30 13:10:15 +0100

    routes and view

--- python_flask/app.py
+++ python_flask/app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, world!'
 
+# Try to create more then one route
+
+@app.route('/contact')
+def contact():
+    return 'Contact us from here!'
+
+@app.route('/home')
+def home():
+    return 'This is the home page!'
+
 if __name__ == '__main__':
     app.run()
